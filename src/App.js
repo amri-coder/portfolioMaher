0b6223f8f9ebe5c3721c29e7df49f2d50febb591
Hafedh Amri <amri_hafedh@yahoo.com>
2021-08-28 20:20:31 +0200

    projet site web perso

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World !</h1>
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3} style={{backgroundColor:'blue'}}>
+          <Profile />
+        </Grid>
+        <Grid item xs style={{backgroundColor:'red'}}>
+          <Header />
+          <Portfolio />
+          <Resume />
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
