c89f6186f37d97431141710d244f414a4c236722
Hafedh Amri <amri_hafedh@yahoo.com>
2021-08-28 22:06:05 +0200

    projet site web perso

--- src/App.js
+++ src/App.js
@@ -5,20 +5,36 @@ import Header from './components/Header/Header';
 import Portfolio from './pages/Portfolio/Portfolio';
 import Resume from './pages/Resume/Resume';
 import Footer from './components/Footer/Footer';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Container>
       <Grid container>
-        <Grid item xs={12} sm={12} md={4} lg={3} style={{backgroundColor:'blue'}}>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
+
           <Profile />
+
         </Grid>
         <Grid item xs style={{backgroundColor:'red'}}>
+
           <Header />
-          <Portfolio />
-          <Resume />
+
+            <Router>
+              <Switch>
+                <Route path="/portfolio">
+                      <Portfolio />
+                </Route>
+                <Route path="/">
+                      <Resume />
+                </Route>
+              </Switch>
+            </Router>
+
           <Footer />
+
         </Grid>
       </Grid>
     </Container>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
+import imageAmri from '../../assets/images/amri.png';
+import CustomTimeline from '../Timeline/Timeline';
+import './Profile.css';
 
 function Profile() {
     return (
-        <div>
-            <h1>Profile !!</h1>
+        <div className='profile container_shadow'>
+            <div className='profile_name'>
+                <Typography className='name'>Amri Maher</Typography>
+                <Typography className='title'>Développeur Web</Typography>
+            </div>
+            <figure className='profile_image'>
+                <img src={imageAmri} alt="Amri" />
+            </figure>
+
+            <div className='profile_information'>
+                <CustomTimeline />
+                <br />
+                <button>My Button</button>
+            </div>
         </div>
     )
 }
