b068c370b9fdcfe4dd051b65b2c5f8eead9cd68b
Hafedh Amri <amri_hafedh@yahoo.com>
2021-08-30 16:26:14 +0200

    projet site web perso

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -5,33 +5,49 @@ import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
+import WorkIcon from '@material-ui/icons/Work';
 import './Timeline.css';
+import { Typography } from '@material-ui/core';
 
-const CustomTimeline = () => {
+const CustomTimeline = ({title,icon, children}) => {
+   
+    
     return (
-        <Timeline>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined"/>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-    </Timeline>
-    )
-}
+        <Timeline className={'timeline'}>
+
+                <TimelineItem className={'timeline_firstItem'}>
+                    <TimelineSeparator>
+                    <TimelineDot className={'timeline_dot_header'}>{<WorkIcon />}</TimelineDot>
+                    <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant='h6' className={'timeline_header'}>
+                            {'title'}
+                        </Typography>
+                    </TimelineContent>
+                </TimelineItem>
+
+
+
+                {children}
+
+
+                <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent>Code</TimelineContent>
+                </TimelineItem>
+                
+                
+
+        </Timeline>
+    );
+};
+
+export const CustomTimelineSeparator =()=> (
+                    <TimelineSeparator className={'separator_padding'}>
+                    <TimelineDot className={'timeline_dot'} />
+                    <TimelineConnector />
+                    </TimelineSeparator>
+)
 
 export default CustomTimeline
