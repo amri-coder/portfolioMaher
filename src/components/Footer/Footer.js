7df4b9c237ab78a465f7d33b0bc120e17b43e4c7
Hafedh Amri <amri_hafedh@yahoo.com>
2021-08-31 21:02:49 +0200

    projet site web perso

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Typography } from '@material-ui/core';
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            Footer !!
+        <div className='footer'>
+            <div className='footer_left'>
+                <Typography className='footer_name'>
+                    Amri Maher.
+                </Typography>
+            </div>
+            <div className='footer_right'>
+                <Typography className='footer_copyright'>
+                Developed by <a href="/" target="_blank">Amri Maher</a>
+                </Typography>
+            </div>
         </div>
     )
 }
