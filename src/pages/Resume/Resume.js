48674331e4ba480649ad2106f0b6abba9453c9b8
Hafedh Amri <amri_hafedh@yahoo.com>
2021-09-03 18:10:15 +0200

    projet site web perso

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CustomTimeline, {CustomTimelineSeparator} from '../../components/Timeline/Timeline';
-import {Grid, Typography} from '@material-ui/core'
+import {Grid, Icon, Paper, Typography} from '@material-ui/core'
 import './Resume.css'
 import resumeData from '../../utils/resumeData'
 import WorkIcon from '@material-ui/icons/Work';
@@ -88,13 +88,48 @@ const Resume = () => {
 
 
         {/* Services */} 
-        <Grid container className='section'>
-                
+        <Grid container className='section pb_45'>
+            <Grid item className='section_title mb_30'>
+                <span></span>
+                <h6 className='section_title_text'>Services</h6>
+            </Grid>  
+
+            <Grid item xs={12}>
+                <Grid container spacing={3} justify='space-around'>
+                    {resumeData.services.map(service=>(
+                        <Grid item xs={12} sm={6} md={3}>
+                            <div className='service'>
+                                <Icon className='service_icon'>{service.icon}</Icon>
+                                <Typography className='service_title' variant='h6'>
+                                    {service.title}
+                                </Typography>
+                                <Typography className='service_description' variant='body2'>
+                                    {service.description}
+                                </Typography>
+                            </div>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
         </Grid>
 
         {/* Skills */} 
-        <Grid container className='section'>
-
+        <Grid container spacing={3} justify='space-between' className='section graybg pb_45'>
+                {resumeData.skills.map(skill=>(
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Paper elevation={0} className='skill'>
+                            <Typography variant='h6' className='skills_title'>
+                                {skill.title}
+                            </Typography>
+                            {skill.description.map(element=>(
+                                <Typography variant='body2' className='skill_description'>
+                                    <TimelineDot variant={'outlined'} className='timeline_dot' />
+                                    {element}
+                                </Typography>
+                            ))}
+                        </Paper>
+                    </Grid>
+                ))}
         </Grid>
 
         {/* Contact */} 
